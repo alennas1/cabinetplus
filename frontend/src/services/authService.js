f23ece2d1194e318d306dc5294af43b547cea799
alennas1 <Alounnas2002@gmail.com>
2025-12-21 09:53:09 +0100

    new variables added

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,8 @@
 // src/services/authService.js
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;const api = axios.create({
+const API_URL = import.meta.env.VITE_API_URL;
+const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // send cookies for refresh token
 });
